def0a4b9d43af7b6dfaced3a16f9369a736eb75e
TatianaMaksakova <tatzianar@gmail.com>
2023-11-10 09:36:03 +0300

    fix errors, add image

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def start_game():
         controls.events(screen, hero, bullets)
         hero.moving_hero(screen)
 
-        controls.update(screen, hero, bullets, enemys)
-        controls.moving_bullets(screen, bullets)
-        controls.create_army(screen)
+        controls.update(screen, hero, enemys, bullets)
+        controls.update_bullets(screen, bullets)
+        controls.create_army(screen,enemys)
 
 start_game()
